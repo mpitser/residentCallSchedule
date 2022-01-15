c97e574f833669aaec173666adfcdede5401dfcc
Mallory Pitser <m.pitser@gmail.com>
2022-01-15 13:31:24 -0500

    adding shells of functions

--- residentCallScheduleProgram.py
+++ residentCallScheduleProgram.py
@@ -1,12 +1,48 @@
+#!/usr/bin/env python3
+
 #inputs: CSV file with resident names, vacation dates, off service months, PGY
 #        CSV file with shifts and days
 #preset parameters: Friday availability, weeks of night float, chunk size of night float, divide holidays evenly
 
 #import CSV files and make data structures
+def main():
+    print "Welcome to the Resident Call Schedule Generator!"
+    importFromCSV()
+    assignHolidays()
+    assignNightFloat()
+    assignFridays()
+    assignOpenShifts()
+    exportToCSV()
+    return
+
+#import data from CSV files
+def importFromCSV():
+    print "Importing data from CSV files"
+    return
 
 #assign holidays
+def assignHolidays():
+    print "Assigning Holidays"
+    return
+    
 #assign night float
+def assignNightFloat():
+    print "Assigning Night Float"
+    return
+    
 #assign Fridays
+def assignFridays():
+    print "Assigning Fridays"
+    return
+
 #divide the rest evenly
+def assignOpenShifts():
+    print "Assigning all open shifts"
+    return
 
 #export data to a CSV file
+def exportToCSV():
+    print "Exporting data to CSV file"
+    return
+
+main()
