2b2a7675b0bce193f45d055d2e84d92394780e4a
Mallory Pitser <mallorypitser@SWA2651.local>
2022-01-15 13:08:59 -0500

    adding program file

---
+++ residentCallScheduleProgram.py
@@ -0,0 +1,12 @@
+#inputs: CSV file with resident names, vacation dates, off service months, PGY
+#        CSV file with shifts and days
+#preset parameters: Friday availability, weeks of night float, chunk size of night float, divide holidays evenly
+
+#import CSV files and make data structures
+
+#assign holidays
+#assign night float
+#assign Fridays
+#divide the rest evenly
+
+#export data to a CSV file
